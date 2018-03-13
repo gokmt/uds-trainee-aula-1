3a4734fca945fabb809a0c7ab3bff7d0c1c4977a
Gustavo <guskamoto@gmail.com>
2018-03-13 15:12:46 -0300

    Aula 02 - JavaScript Exercício-01-A-Slide

--- Aula 02 - JavaScript/Exercícios Slide/js/script.js
+++ Aula 02 - JavaScript/Exercícios Slide/js/script.js
@@ -22,3 +22,33 @@ if (idade <= 18) {
 }else{
   alert("Tiozão(ona)");
 }
+
+/*Exercício 4*/
+var dia = new Date().getDay();
+var diaSemana;
+switch (dia) {
+  case 0:
+    diaSemana = "Domingo";
+  break;
+  case 1:
+    diaSemana = "Segunda-Feira";
+  break;
+  case 2:
+    diaSemana = "Terça-Feira";
+  break;
+  case 3:
+    diaSemana = "Quarta-Feira";
+  break;
+  case 4:
+    diaSemana = "Quinta-Feira";
+  break;
+  case 5:
+    diaSemana = "Sexta-Feira";
+  break;
+  case 2:
+    diaSemana = "Sábado";
+  break;
+  default:
+
+}
+alert(diaSemana);
