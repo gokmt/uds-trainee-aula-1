56ee3c4bdcf59a403399240533517382d5180956
Gustavo <guskamoto@gmail.com>
2018-03-13 14:48:45 -0300

    Aula 02 - JavaScript Exercício-01-B-Slide

--- Aula 02 - JavaScript/Exercícios Slide/js/script.js
+++ Aula 02 - JavaScript/Exercícios Slide/js/script.js
@@ -1,6 +1,12 @@
 /*Exercício 1-A*/
-var nomeA = "Gustavo";
+/*var nomeA = "Gustavo";
 var nomeB = Gustavo;
 
 alert(nomeA);
-alert(nomeB);
+alert(nomeB);*/
+
+/*Exercício 2-B*/
+var nome = "Gustavo Keith Okamoto";
+var idade = 22;
+var sexoFeminino = false;
+var peso = 100.00;
